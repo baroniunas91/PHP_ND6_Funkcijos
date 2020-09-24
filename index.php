359397da1c5eca1416775ac4a6e7a78599822615
Edgaras <baroniunas@gmail.com>
2020-09-24 15:01:57 +0300

    second task done

--- index.php
+++ index.php
@@ -7,4 +7,15 @@ function insertText($string) {
     echo "<h1>$string</h1>";
 }
 insertText('Hello World!');
-
+echo '<br>';
+echo '2) ----------------------------------------------------------------------------------------------------';
+echo '<br>';
+// Parašykite funkciją su dviem argumentais, pirmas argumentas tekstas, įterpiamas į h tagą, o antrasis 
+// tago numeris (1-6). Rašydami šią funkciją remkitės pirmame uždavinyje parašytą funkciją;
+function insertTextAndTag($string, $tagNum) {
+    echo "<h$tagNum>$string</h$tagNum>";
+}
+insertTextAndTag('Stringas su Tag\'u', rand(1,6));
+echo '<br>';
+echo '3) ----------------------------------------------------------------------------------------------------';
+echo '<br>';
