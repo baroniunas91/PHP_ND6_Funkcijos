1c0a814c629fe35bd4a40f3db1617fcedd3d8fb9
Edgaras <baroniunas@gmail.com>
2020-09-24 14:57:54 +0300

    first task done

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 echo '1) ----------------------------------------------------------------------------------------------------';
 echo '<br>';
+// Parašykite funkciją, kurios argumentas būtų tekstas, kuris yra įterpiamas į h1 tagą;
+
+function insertText($string) {
+    echo "<h1>$string</h1>";
+}
+insertText('Hello World!');
+
